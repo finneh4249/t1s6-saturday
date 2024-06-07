d83c33243af62fd0bc1bac43c92d6bffa3248a6b
Ethan Cornwill <ethancornwill@outlook.com>
2024-06-08 09:57:09 +1000

    Comments

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 ## If-Else Practice Activiy
 
-### Building a Calculator
+### Building a Calculator ([calculator.py](calculator.py))
 
 Build a calculator that takes 3 inputs from the user:
 2 inputs as numbers (floats) for operation and 3rd input to decide which operation they want to perform.
 
-## Lists
+## Lists [list_example.py](list_example.py)
 
 Composite data type, meaning multiple data types can be stored in one variable.
 Each piece is called an element.
--- list_example.py
+++ list_example.py
@@ -8,17 +8,27 @@
 # print(country3)
 # print(country4)
 
+# list of countries
 country_list = ["Australia", "India", "New Zealand", "England"]
+# print the list
 print(country_list)
+# print the second element in the list (India)
 print(country_list[1])
 
+# change the second element in the list (India) to Nepal
 country_list[1] = "Nepal"
+# print the updated list
 print(country_list)
 
+# append Japan to the list
 country_list.append('Japan')
+# print the updated list
 print(country_list)
 
+# remove England from the list
 country_list.remove('England')
+# print the updated list
 print(country_list)
 
+# print the total number of elements in the list
 print("Total length of the list: ", len(country_list))
